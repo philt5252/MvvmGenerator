f876b163f7ad21f702cf9ffd46bc9e7604d9f079
mregan59 <mregan59@gmail.com>
2014-05-17 21:04:20 -0500

    scaffoldCommandParser

---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Services/Parsers/ScaffoldCommandParser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Olf.MvvmGenerator.Foundation.Models;
+using Olf.MvvmGenerator.Foundation.Services.Parsers;
+
+namespace Olf.MvvmGenerator.Core.Services.Parsers
+{
+    public class ScaffoldCommandParser : IScaffoldCommandParser
+    {
+        private PropertyDetailsParser propertyDetailsParser;
+        public ScaffoldCommandParser()
+        {
+            propertyDetailsParser = new PropertyDetailsParser();
+        }
+        public bool CheckValidCommand(string command)
+        {
+            if (command.StartsWith("Scaffold"))
+            {
+                return false;
+            }
+            try
+            {
+                Parse(command);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public ParsedScaffoldCommand Parse(string command)
+        {
+            String[] parsedCommand = command.Split(' ');
+            ParsedScaffoldCommand parsedModelCommand = new ParsedScaffoldCommand();
+            parsedModelCommand.Command = parsedCommand[0];
+            parsedModelCommand.ObjectName = parsedCommand[1];
+
+            List<PropertyDetails> propertyDetails = propertyDetailsParser.
+                ParsePropertyDetails(parsedCommand.Skip(2).ToArray());
+            parsedModelCommand.Properties = propertyDetails;
+
+            return parsedModelCommand;
+        }
+    }
+    
+}
---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Foundation/Services/Parsers/IScaffoldCommandParser.cs
@@ -0,0 +1,10 @@
+﻿using Olf.MvvmGenerator.Foundation.Models;
+
+namespace Olf.MvvmGenerator.Foundation.Services.Parsers
+{
+    public interface IScaffoldCommandParser
+    {
+        bool CheckValidCommand(string command);
+        ParsedScaffoldCommand Parse(string command);
+    }
+}

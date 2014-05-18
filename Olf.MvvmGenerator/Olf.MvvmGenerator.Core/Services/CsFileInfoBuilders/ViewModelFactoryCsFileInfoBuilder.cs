a47542d680b3cbee83ffaafa8b7d9342faf19ffd
mregan59 <mregan59@gmail.com>
2014-05-17 22:20:53 -0500

    viewModel CsFileBuilders

---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Services/CsFileInfoBuilders/ViewModelFactoryCsFileInfoBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using Olf.Common.VisualStudio;
+using Olf.MvvmGenerator.Core.Services.Generators;
+using Olf.MvvmGenerator.Foundation.Models;
+
+namespace Olf.MvvmGenerator.Core.Services.CsFileInfoBuilders
+{
+    public class ViewModelFactoryCsFileInfoBuilder : CsFileInfoBuilder<ParsedViewModelCommand>
+    {
+          private readonly CsFileInfo viewModelInterfaceCsFileInfo;
+          private readonly CsFileInfo viewModelFactoryInterfaceCsFileInfo;
+
+        public ViewModelFactoryCsFileInfoBuilder(ParsedViewModelCommand parsedCommand, IVisualStudioIde visualStudioIde,
+            CsFileInfo viewModelInterfaceCsFileInfo, CsFileInfo viewModelFactoryInterfaceCsFileInfo) 
+            : base(parsedCommand, visualStudioIde)
+        {
+            this.viewModelInterfaceCsFileInfo = viewModelInterfaceCsFileInfo;
+            this.viewModelFactoryInterfaceCsFileInfo = viewModelFactoryInterfaceCsFileInfo;
+        }
+
+        protected override string CreateProjectName(string[] projectNames)
+        {
+            string projectName = projectNames.FirstOrDefault(p => p.EndsWith(".Core"));
+            projectName = projectName ?? projectNames.First();
+
+            return projectName;
+        }
+
+        public override void CreateProperties()
+        {
+            //base.CreateProperties();
+        }
+
+        protected override string CreateObjectName(ParsedViewModelCommand parsedCommand)
+        {
+            return parsedCommand.ObjectName + "Factory";
+        }
+
+        protected override string CreateFilePath(ParsedViewModelCommand parsedCommand)
+        {
+            return string.Format("Factories\\ViewModels\\{0}", CsFileInfo.FileName);
+        }
+
+        public override void CreateUsings()
+        {
+            base.CreateUsings();
+
+            CsFileInfo.Usings.Add(viewModelInterfaceCsFileInfo.Namespace);
+            CsFileInfo.Usings.Add(viewModelFactoryInterfaceCsFileInfo.Namespace);
+
+            CsFileInfo.Usings = CsFileInfo.Usings.Distinct().ToList();
+        }
+
+        public override void CreateBaseClass()
+        {
+            CsFileInfo.BaseClass = viewModelFactoryInterfaceCsFileInfo.ObjectName;
+        }
+    }
+}
---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Services/CsFileInfoBuilders/ViewModelFactoryInterfaceCsFileInfoBuilder.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Olf.Common.VisualStudio;
+using Olf.MvvmGenerator.Core.Services.Generators;
+using Olf.MvvmGenerator.Foundation.Models;
+
+namespace Olf.MvvmGenerator.Core.Services.CsFileInfoBuilders
+{
+    public class ViewModelFactoryInterfaceCsFileInfoBuilder : CsFileInfoBuilder<ParsedViewModelCommand>
+    {
+        private readonly CsFileInfo viewModelInterfaceCsFileInfo;
+
+         public ViewModelFactoryInterfaceCsFileInfoBuilder(ParsedViewModelCommand parsedCommand, IVisualStudioIde visualStudioIde,
+            CsFileInfo viewModelInterfaceCsFileInfo) 
+            : base(parsedCommand, visualStudioIde)
+        {
+            this.viewModelInterfaceCsFileInfo = viewModelInterfaceCsFileInfo;
+        }
+
+        protected override string CreateProjectName(string[] projectNames)
+        {
+            string projectName = projectNames.FirstOrDefault(p => p.EndsWith(".Foundation"));
+            projectName = projectName ?? projectNames.First();
+
+            return projectName;
+        }
+
+        public override void CreateProperties()
+        {
+            //base.CreateProperties();
+        }
+
+        protected override string CreateObjectName(ParsedViewModelCommand parsedCommand)
+        {
+            return "I" + parsedCommand.ObjectName + "Factory";
+        }
+
+        protected override string CreateFilePath(ParsedViewModelCommand parsedCommand)
+        {
+            return string.Format("Factories\\ViewModels\\{0}", CsFileInfo.FileName);
+        }
+
+        public override void CreateUsings()
+        {
+            base.CreateUsings();
+
+            CsFileInfo.Usings.Add(viewModelInterfaceCsFileInfo.Namespace);
+        }
+    }
+}

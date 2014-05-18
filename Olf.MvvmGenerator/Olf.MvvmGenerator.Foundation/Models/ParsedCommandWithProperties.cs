e2edf65919fbd5583b49eb816c59cc3fbf459ad1
mregan59 <mregan59@gmail.com>
2014-05-17 20:24:00 -0500

    parsedModelCommand

---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Foundation/Models/ParsedCommandWithProperties.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Olf.MvvmGenerator.Foundation.Models
+{
+    public class ParsedCommandWithProperties 
+    {
+        public String Command { get; set; }
+        public String ObjectName { get; set; }
+        public List<PropertyDetails> Properties { get; set; }
+    }
+}
--- Olf.MvvmGenerator/Olf.MvvmGenerator.Foundation/Models/ParsedModelCommand.cs
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Foundation/Models/ParsedModelCommand.cs
@@ -1,7 +1,11 @@
-﻿namespace Olf.MvvmGenerator.Foundation.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace Olf.MvvmGenerator.Foundation.Models
 {
-    public class ParsedModelCommand
+    public class ParsedModelCommand : ParsedCommandWithProperties
     {
+        
          
     }
 }

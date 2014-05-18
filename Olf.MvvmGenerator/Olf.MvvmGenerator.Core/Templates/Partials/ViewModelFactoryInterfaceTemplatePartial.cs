72f5c161d9a71d9418b2ae4eb8e7eace86cd8797
philt5252 <philt5252@gmail.com>
2014-05-17 22:31:57 -0500

    working on viewModels

---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Templates/Partials/ViewModelFactoryInterfaceTemplatePartial.cs
@@ -0,0 +1,17 @@
+﻿using Olf.MvvmGenerator.Core.Services.Generators;
+using Olf.MvvmGenerator.Foundation.Templates;
+
+namespace Olf.MvvmGenerator.Core.Templates
+{
+    public partial class ViewModelFactoryInterfaceTemplate : IFileTemplate
+    {
+        protected readonly CsFileInfo csFileInfo;
+        protected readonly string interfaceObjectName;
+
+        public ViewModelFactoryInterfaceTemplate(CsFileInfo csFileInfo)
+        {
+            this.csFileInfo = csFileInfo;
+            interfaceObjectName = csFileInfo.ObjectName.Replace("Factory", "");
+        }
+    }
+}
---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Templates/Partials/ViewModelTemplatePartial.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Olf.MvvmGenerator.Core.Services.Generators;
+using Olf.MvvmGenerator.Foundation.Templates;
+
+namespace Olf.MvvmGenerator.Core.Templates
+{
+    public partial class ViewModelTemplate : IFileTemplate
+    {
+        protected readonly CsFileInfo csFileInfo;
+
+        public ViewModelTemplate(CsFileInfo csFileInfo)
+        {
+            this.csFileInfo = csFileInfo;
+        }
+    }
+}

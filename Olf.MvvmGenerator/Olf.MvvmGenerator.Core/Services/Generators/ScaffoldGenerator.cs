9b88e5bcf2f7bd7e616c299dce13ad754c32e0e7
mregan59 <mregan59@gmail.com>
2014-05-17 22:46:03 -0500

    runner and generator for scaffoldCommand

---
+++ Olf.MvvmGenerator/Olf.MvvmGenerator.Core/Services/Generators/ScaffoldGenerator.cs
@@ -0,0 +1,13 @@
+﻿using Olf.MvvmGenerator.Foundation.Models;
+using Olf.MvvmGenerator.Foundation.Services.Generators;
+
+namespace Olf.MvvmGenerator.Core.Services.Generators
+{
+    public class ScaffoldGenerator : IScaffoldGenerator
+    {
+        public void Run(ParsedScaffoldCommand parsedModelCommand)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
